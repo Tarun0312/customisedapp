574808fddc6e26ba0bd91f13e0c292dc1363a122
Tarun0312 <tarunsardana33@gmail.com>
2025-01-12 19:11:53 +0530

    header updated

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -2,17 +2,10 @@ import { Component } from '@angular/core';
 import { ThemeService } from '../../../app/services/theme.service';
 
 @Component({
-  selector: 'custom-header',
+  selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent {
-  theme = this.themeService.getTheme();
 
-  constructor(private themeService: ThemeService) {}
-
-  changeTheme(theme: string): void {
-    this.themeService.setTheme(theme);
-    this.theme = theme;
-  }
 }
